408bb45c854c2f8f0879c0f196e797ed4b3a3417
Richard Gieg <richgieg@users.noreply.github.com>
2015-12-04 01:52:30 -0800

    Enable HTML email body

--- app/email.py
+++ app/email.py
@@ -6,7 +6,6 @@ def send_email(to, subject, template, **kwargs):
     app = current_app._get_current_object()
     sender = app.config['APP_MAIL_SENDER']
     subject = app.config['APP_MAIL_SUBJECT_PREFIX'] + ' ' + subject
-    body = render_template(template + '.html', **kwargs)
-    # msg.body = render_template(template + '.txt', **kwargs)
-    # msg.html = render_template(template + '.html', **kwargs)
-    mail.send_mail(sender, to, subject, body)
+    body = render_template(template + '.txt', **kwargs)
+    html = render_template(template + '.html', **kwargs)
+    mail.send_mail(sender, to, subject, body, html=html)
